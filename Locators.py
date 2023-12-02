1e490bb3045a235b8a92857e6287a8c4b7339251
reihaanahad <143589424+reihaanahad@users.noreply.github.com>
2023-12-02 17:41:16 +0330

    Update Locators.py

--- Locators.py
+++ Locators.py
@@ -13,5 +13,95 @@ login_password_submit = "//*[@id='password-submit-button']"
 login_link_phone_edit = "//*[@id='root']/div/div/div/div/div[3]"
 login_link_forget = "//*[@id='mobile']/div[2]/div[3]"
 login_link_otp_login = "//*[@id='otp-login-button']"
+login_wrong_password = "//*[@id='mobile_password_help']/div"
+login_hidden_password = "//*[@id='mobile']/div[2]/div[1]/div[1]/div[2]/div[1]/div/span/span"
+login_account_button = "//*[@id='root']/div/div/header[1]/nav/div/div[2]/div[3]/div/div/div"
 
+#thirdparty
+
+thirdparty_ins_icon = "//*[@id='root']/div/div/main/main/div[1]/div/div[2]/a[1]"
+thirdparty_forget_car_tag = "//*[@id='root']/div/div/main/div/main/div[1]/div/div[2]/div[1]/div/div/form/div[2]/div[2]/button"
+thirdparty_vehicle_category = "//*[@id='vehicleCategoryId']"
+thirdparty_using_type = "//*[@id='usingType']"
+thirdparty_brands = "//*[@id='brands']"
+thirdparty_production_year = "//*[@id='productionYear']"
+thirdparty_continue_discounts_detail= "//*[@id='root']/div/div/main/div/main/div[1]/div/div[2]/div[1]/div/div/form/div[3]/div[1]/div/button"
+thirdparty_previous_ins_status= "//*[@id='previousInsuranceStatus']"
+thirdparty_owner_change_yes = "//*[@id='ownershipChange']/label[1]"
+thirdparty_owner_change_no = "//*[@id='ownershipChange']/label[2]"
+thirdparty_continue_ins_detail= "//*[@id='root']/div/div/main/div/main/div[1]/div/div[2]/div[1]/section/div/div/div/div/form/div[2]/div[2]/button"
+thirdparty_previous_company = "//*[@id='previousCompany']"
+thirdparty_previous_duration = "//*[@id='previousDuration']"
+thirdparty_discount = "//*[@id='thirdPartyDiscount']"
+thirdparty_driver_discount = "//*[@id='driverDiscount']"
+thirdparty_damage_yes = "//*[@id='damage']/label[2]"
+thirdparty_damage_no = "//*[@id='damage']/label[2]"
+
+#motor
+
+
+motor_ins_icon = "//*[@id='root']/div/div/main/main/div[1]/div/div[2]/a[2]"
+motor_type = "//*[@id='motorType']"
+motor_production_year = "//*[@id='productionYear']"
+motor_ins_detail = "//*[@id='root']/div/div/main/main/div[1]/div/div[2]/div[1]/section/div/div/form/div[2]/div/button"
+motor_previous_ins_status= "//*[@id='previousInsuranceStatus']"
+motor_previous_company = "//*[@id='previousCompany']"
+motor_previous_duration = "//*[@id='previousDuration']"
+motor_discount = "//*[@id='thirdPartyDiscount']"
+motor_driver_discount = "//*[@id='driverDiscount']"
+
+#body
+
+body_ins_icon = "//*[@id='root']/div/div/main/main/div[1]/div/div[2]/a[3]"
+body_vehicle_category = "//*[@id='vehicleCategoryId']"
+body_using_type = "//*[@id='usingType']"
+body_brands = "//*[@id='brands']"
+body_model = "//*[@id='models']"
+body_price = "//*[@id='price']"
+body_has_insurance = "//*[@id='hasInsurance']/label[1]"
+body_damage_year = "//*[@id='noDamageYear']"
+body_import = "//*[@id='import']/label[1]"
+body_build_year = "//*[@id='buildYear']"
+body_build_month = "//*[@id='buildMonth']"
+body_company = "//*[@id='company']"
+body_discount = "//*[@id='thirdPartyDiscount']"
+
+#fire
+
+fire_ins_icon = "//*[@id='root']/div/div/main/main/div[1]/div/div[2]/a[4]"
+fire_estate_type = "//*[@id='estateType']"
+fire_structure_type = "//*[@id='structureType']/label[1]"
+fire_total_area = "//*[@id='totalArea']"
+fire_area_unit_price = "//*[@id='areaUnitPrice']"
+
+#earthquake
+
+earthquake_ins_icon = "//*[@id='root']/div/div/main/main/div[1]/div/div[2]/a[5]"
+earthquake_estate_type = "//*[@id='estateType']"
+earthquake_structure_type = "//*[@id='structureType']/label[1]"
+earthquake_total_area = "//*[@id='totalArea']"
+earthquake_home_appliances_value = "//*[@id='homeAppliancesValue']"
+earthquake_area_unit_price = "//*[@id='areaUnitPrice']"
+
+ #holyshrines
+
+holyshrines_ins_icon = "//*[@id='root']/div/div/main/main/div[1]/div/div[2]/a[6]"
+
+#health
+
+health_ins_icon = "//*[@id='root']/div/div/main/main/div[1]/div/div[2]/a[7]"
+health_day_of_birth = "//*[@id='day0']"
+health_month_of_birth = "//*[@id='month0']"
+health_year_of_birth = "//*[@id='year0']"
+health_basic_insurer = "//*[@id='basicInsurer']"
+
+#travel
+
+travel_ins_icon = "//*[@id='root']/div/div/main/main/div[1]/div/div[2]/a[8]"
+travel_countries = "//*[@id='searchbox_travel']/div[1]/div[1]/div/div/div[2]/div/div/div/div"
+travel_duration = "//*[@id='searchbox_travel_duration']"
+
+#travelplus
+
+travel_plus_ins_icon = "//*[@id='root']/div/div/main/main/div[1]/div/div[2]/a[9]"
 
